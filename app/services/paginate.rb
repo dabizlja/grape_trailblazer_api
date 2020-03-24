34b204e46e6d76b60a8e65b5089e04a6405ec09b
Dabizlja <dabizlja@gmail.com>
2020-03-24 17:22:04 +0100

    Added sanitize_sql for query sanitization

--- app/services/paginate.rb
+++ app/services/paginate.rb
@@ -15,9 +15,8 @@ class Paginate
 
   def paginate_projects
     query = "SELECT * FROM projects ORDER BY #{@sort_attr} #{@order} LIMIT #{@per} OFFSET #{offset(@page, @per)}"
-    connect = ActiveRecord::Base.connection();
-    sanitized_query = ActiveRecord::Base.send(:sanitize_sql_array, query)
-    connect.execute(sanitized_query)
+    sanitized_query = ActiveRecord::Base.sanitize_sql(query)
+    ActiveRecord::Base.connection.execute(sanitized_query)
   end
 
   def offset(page, per_page)
